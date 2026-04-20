ca52c8b58e7bca070044cee60895cdfdc243c858
derpapitem <derpapitem@gmail.com>
2026-04-20 14:30:45 +0200

    v08

--- models/mart/mart_sales_performance.sql
+++ models/mart/mart_sales_performance.sql
@@ -7,7 +7,7 @@ SELECT
     category_name,
     SUM(revenue) AS total_revenue,
     COUNT(DISTINCT order_id) AS total_orders,
-    AVG(revenue) AS avg_revenue_per_order
+    ROUND(AVG(revenue),2) AS avg_revenue_per_order
 FROM sales
 GROUP BY 1, 2, 3
 ORDER BY category_name, order_year, order_month
